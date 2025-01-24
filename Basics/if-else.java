e1c03cc1d9dd16bd036b2e4d6c3179377badf2a2
dasmiita <185056260+dasmiita@users.noreply.github.com>
2025-01-24 15:52:50 +0530

    Add files via upload

---
+++ Basics/if-else.java
@@ -0,0 +1,13 @@
+class Riya
+{
+    public static void main(String[] args)
+    {
+        int num = 4;
+        if(num==0)
+            System.out.println("It is zero.");
+        else if(num>0)
+            System.out.println("It is positive.");
+        else if(num<0)
+            System.out.println("It is negative.");  
+    }
+}
