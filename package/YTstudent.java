778b6a8067e094082e12578083e7f3758afaa015
dasmiita <185056260+dasmiita@users.noreply.github.com>
2025-02-11 19:37:24 +0530

    Add files via upload

---
+++ package/YTstudent.java
@@ -0,0 +1,11 @@
+package library;
+public class YTstudent {
+    public String name;
+    public int roll;
+    public double marks;
+    public void display(){
+        System.out.println("your name is  = " +name);
+        System.out.println("your roll is  = " +roll);
+        System.out.println("your marks is  = " +marks);
+    }
+}
