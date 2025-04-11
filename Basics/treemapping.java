f8c28e2cc78fbd3b8f5f8dbdfc8d9df7f5e91a8a
dasmiita <185056260+dasmiita@users.noreply.github.com>
2025-04-11 10:01:42 +0530

    Add files via upload

---
+++ Basics/treemapping.java
@@ -0,0 +1,17 @@
+package newProject;
+
+import java.util.*;
+public class treemapping
+{
+	public static void main(String[] args)
+	{
+		TreeMap<String, Integer> map = new TreeMap<>(); //inorder traversal
+	
+			
+		map.put("A", 40);
+		map.put("b", 54);
+		map.put("c", 39);
+		map.put("d", 18);
+		
+		System.out.println(map);
+	}}
